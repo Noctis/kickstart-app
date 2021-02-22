11c34f04a7a0d0c6e77dbffea1daee17ab7af518
Lucas Czejgis <lucas.czejgis@gmail.com>
2021-02-22 13:40:16 +0100

    Moved AbstractTable class from kickstart.

--- src/Database/Table/DummyTable.php
+++ src/Database/Table/DummyTable.php
@@ -1,8 +1,6 @@
 <?php declare(strict_types=1);
 namespace App\Database\Table;
 
-use Noctis\KickStart\Database\Table\AbstractTable;
-
 final class DummyTable extends AbstractTable
 {
     public const NAME = 'dummy';
