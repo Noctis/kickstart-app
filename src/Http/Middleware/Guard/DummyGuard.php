729657acc880701714c9ebaa26fcf43e3494e7c2
Lucas Czejgis <lucas.czejgis@gmail.com>
2021-04-15 15:54:18 +0200

    Removed some leftover code.

--- src/Http/Middleware/Guard/DummyGuard.php
+++ src/Http/Middleware/Guard/DummyGuard.php
@@ -4,24 +4,20 @@ declare(strict_types=1);
 
 namespace App\Http\Middleware\Guard;
 
-use Noctis\KickStart\Configuration\ConfigurationInterface;
 use Noctis\KickStart\Http\Helper\HttpRedirectionTrait;
 use Noctis\KickStart\Http\Middleware\AbstractMiddleware;
 use Noctis\KickStart\Http\Middleware\RequestHandlerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-/** @psalm-suppress PropertyNotSetInConstructor */
 final class DummyGuard extends AbstractMiddleware
 {
     use HttpRedirectionTrait;
 
-    private ConfigurationInterface $configuration;
     private bool $dummyParam;
 
-    public function __construct(ConfigurationInterface $configuration, bool $dummyParam)
+    public function __construct(bool $dummyParam)
     {
-        $this->configuration = $configuration;
         $this->dummyParam = $dummyParam;
     }
 
