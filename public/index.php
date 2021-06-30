a0f9dc256c104012b9602d50321860f4c14d6669
Lucas Czejgis <lucas.czejgis@gmail.com>
2021-06-30 14:44:26 +0200

    Refactored HTTP app class & moved related service providers to index.php

--- public/index.php
+++ public/index.php
@@ -3,10 +3,28 @@
 declare(strict_types=1);
 
 use App\Http\Application;
+use App\Provider\ConfigurationProvider;
+use App\Provider\DatabaseConnectionProvider;
+use App\Provider\DummyServicesProvider;
+use App\Provider\HttpMiddlewareProvider;
+use App\Provider\RepositoryProvider;
+use Noctis\KickStart\Http\ContainerBuilder;
 
 require_once __DIR__ . '/../bootstrap.php';
 
-$app = new Application(
+$containerBuilder = new ContainerBuilder();
+$containerBuilder
+    ->registerServicesProvider(new ConfigurationProvider())
+    ->registerServicesProvider(new DatabaseConnectionProvider())
+    ->registerServicesProvider(new HttpMiddlewareProvider())
+    ->registerServicesProvider(new DummyServicesProvider())
+    ->registerServicesProvider(new RepositoryProvider())
+;
+$container = $containerBuilder->build();
+
+/** @var Application $app */
+$app = $container->get(Application::class);
+$app->setRoutes(
     require_once __DIR__ . '/../src/Http/Routing/routes.php'
 );
 $app->run();
