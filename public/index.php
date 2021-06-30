98af1bd07853da75c61f074f9261f4b2cb076fe2
Lucas Czejgis <lucas.czejgis@gmail.com>
2021-06-30 14:54:19 +0200

    Removed HTTP's `Application` class.

--- public/index.php
+++ public/index.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
-use App\Http\Application;
 use App\Provider\ConfigurationProvider;
 use App\Provider\DatabaseConnectionProvider;
 use App\Provider\DummyServicesProvider;
 use App\Provider\HttpMiddlewareProvider;
 use App\Provider\RepositoryProvider;
 use Noctis\KickStart\Http\ContainerBuilder;
+use Noctis\KickStart\Http\WebApplication;
 
 require_once __DIR__ . '/../bootstrap.php';
 
@@ -22,8 +22,8 @@ $containerBuilder
 ;
 $container = $containerBuilder->build();
 
-/** @var Application $app */
-$app = $container->get(Application::class);
+/** @var WebApplication $app */
+$app = $container->get(WebApplication::class);
 $app->setRoutes(
     require_once __DIR__ . '/../src/Http/Routing/routes.php'
 );
