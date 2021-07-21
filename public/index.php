94836d1742f395dc740625fa612325a837e70099
Lucas Czejgis <lucas.czejgis@gmail.com>
2021-07-21 15:20:51 +0200

    Core Refactor (#8)
    * Refactored HTTP app class & moved related service providers to index.php
    * Removed HTTP's `Application` class.
    * Refactored console to use new `ConsoleApplication` & `ContainerBuilder`.
    * Utilizing new HTTP routes loading method.
    * Simplified loading configuration from .env
    * Got rid of global `$env` variable (from `bootstrap.php`)
    * DIC will now be faster in production (`debug=false` in `.env`) env.
    * `FancyConfiguration` is no longer `Configuration`'s decorator.
    * Utilizing new `RoutingProvider` to load app's routes.
    * Replaced a call to `$_ENV` with a call to `Configuration::get()`.
    * Updated documentation.
    * Routes are now an array of `Route` objects, not a simple array.
    * Switched to `noctis/kickstart` 3.0-dev

--- public/index.php
+++ public/index.php
@@ -2,11 +2,37 @@
 
 declare(strict_types=1);
 
-use App\Http\Application;
+use App\Provider\ConfigurationProvider;
+use App\Provider\DatabaseConnectionProvider;
+use App\Provider\DummyServicesProvider;
+use App\Provider\HttpMiddlewareProvider;
+use App\Provider\RepositoryProvider;
+use Noctis\KickStart\Configuration\Configuration;
+use Noctis\KickStart\Http\ContainerBuilder;
+use Noctis\KickStart\Http\WebApplication;
+use Noctis\KickStart\Provider\RoutingProvider;
 
 require_once __DIR__ . '/../bootstrap.php';
 
-$app = new Application(
-    require_once __DIR__ . '/../src/Http/Routing/routes.php'
-);
+$containerBuilder = new ContainerBuilder();
+$containerBuilder
+    ->registerServicesProvider(new RoutingProvider(
+        require_once __DIR__ . '/../src/Http/Routing/routes.php'
+    ))
+    ->registerServicesProvider(new ConfigurationProvider())
+    ->registerServicesProvider(new DatabaseConnectionProvider())
+    ->registerServicesProvider(new HttpMiddlewareProvider())
+    ->registerServicesProvider(new DummyServicesProvider())
+    ->registerServicesProvider(new RepositoryProvider())
+;
+if (Configuration::isProduction()) {
+    /** @var string */
+    $basePath = Configuration::get('basepath');
+    $containerBuilder->enableCompilation($basePath . '/var/cache/container');
+}
+
+$container = $containerBuilder->build();
+
+/** @var WebApplication $app */
+$app = $container->get(WebApplication::class);
 $app->run();
