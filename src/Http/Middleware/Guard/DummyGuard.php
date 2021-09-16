8e8dd31b6c899d9158c4e10b4a6790fe6572d4ba
Lucas Czejgis <lucas.czejgis@gmail.com>
2021-09-16 13:00:58 +0200

    Updated docs & code regarding removal of `AbstractMiddleware`. (#21)

--- src/Http/Middleware/Guard/DummyGuard.php
+++ src/Http/Middleware/Guard/DummyGuard.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace App\Http\Middleware\Guard;
 
-use Noctis\KickStart\Http\Middleware\AbstractMiddleware;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-final class DummyGuard extends AbstractMiddleware
+final class DummyGuard implements MiddlewareInterface
 {
     private bool $dummyParam;
 
@@ -22,6 +22,6 @@ final class DummyGuard extends AbstractMiddleware
     {
         // inspect $request, do stuff, maybe return a RedirectResponse instance?
 
-        return parent::process($request, $handler);
+        return $handler->handle($request);
     }
 }
