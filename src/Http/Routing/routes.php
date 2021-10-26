293283982da84a2098c95c8a4ce70b19239f4b5c
Lucas Czejgis <lucas.czejgis@gmail.com>
2021-10-26 12:21:14 +0200

    Added some example routes, with named parameters.

--- src/Http/Routing/routes.php
+++ src/Http/Routing/routes.php
@@ -8,4 +8,10 @@ use Noctis\KickStart\Http\Routing\Route;
 
 return [
     Route::get('/', DummyAction::class, [DummyGuard::class]),
+
+    // Example route: the `id` parameter is required and must be a number
+    //Route::get('/user/{id:\d+}', DummyAction::class),
+
+    // Example route: the `title` parameter is required (as is the `/` in front of it)
+    //Route::get('/project[/{title}]', DummyAction::class),
 ];
