d6a9a5f4b3c72affe95a66f21dd754d84f6aacad
Lucas Czejgis <lucas.czejgis@gmail.com>
2022-07-19 15:24:01 +0200

    Custom requests are now inserted through the `execute` method

--- src/Http/Request/DummyRequest.php
+++ src/Http/Request/DummyRequest.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace App\Http\Request;
 
-use Noctis\KickStart\Http\Request\AbstractRequest;
+use Noctis\KickStart\Http\Request\Request;
+use Psr\Http\Message\ServerRequestInterface;
 
-final class DummyRequest extends AbstractRequest
+final class DummyRequest extends Request implements ServerRequestInterface
 {
     public function getFoo(): string
     {
