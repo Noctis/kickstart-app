250bbb6604acd952475080cbeb132c20ff67ff1c
Lucas Czejgis <lucas.czejgis@gmail.com>
2022-07-19 15:24:01 +0200

    Custom requests are now inserted through the `execute` method

--- src/Http/Routing/routes.php
+++ src/Http/Routing/routes.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 use App\Http\Action\DummyAction;
 use App\Http\Middleware\Guard\DummyGuard;
+use App\Http\Request\DummyRequest;
 use Noctis\KickStart\Http\Routing\Route;
 
 return [
-    Route::get('/', DummyAction::class, [DummyGuard::class]),
+    Route::get('/', DummyAction::class, [DummyGuard::class], DummyRequest::class),
 
     // Example route: the `id` parameter is required and must be a number
     //Route::get('/user/{id:\d+}', DummyAction::class),
