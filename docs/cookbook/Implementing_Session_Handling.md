59349d6d63d538a8b9a19a733e795f7de2f99895
Lucas Czejgis <lucas.czejgis@gmail.com>
2022-12-09 11:34:31 +0100

    Some standard files were evading Psalm's inspections (fixed that) (#93)

--- docs/cookbook/Implementing_Session_Handling.md
+++ docs/cookbook/Implementing_Session_Handling.md
@@ -52,14 +52,10 @@ declare(strict_types=1);
 
 // ...
 use App\Provider\SessionProvider;
-use Noctis\KickStart\Configuration\Configuration;
 use Noctis\KickStart\Http\WebApplication;
-use Noctis\KickStart\Provider\RoutingProvider;
 
 require_once __DIR__ . '/../bootstrap.php';
 
-/** @var $basePath string */
-$basePath = Configuration::get('basepath');
 $app = WebApplication::boot([
     // ...
     new SessionProvider()
--- public/index.php
+++ public/index.php
@@ -6,14 +6,12 @@ use App\Provider\DatabaseConnectionProvider;
 use App\Provider\DummyServicesProvider;
 use App\Provider\HttpMiddlewareProvider;
 use App\Provider\RepositoryProvider;
-use Noctis\KickStart\Configuration\Configuration;
+use Noctis\KickStart\Http\Routing\RouteInterface;
 use Noctis\KickStart\Http\WebApplication;
 use Noctis\KickStart\Provider\RoutingProvider;
 
 require_once __DIR__ . '/../bootstrap.php';
 
-/** @var $basePath string */
-$basePath = Configuration::get('basepath');
 $app = WebApplication::boot([
     new RoutingProvider(),
     new DatabaseConnectionProvider(),
@@ -21,7 +19,8 @@ $app = WebApplication::boot([
     new DummyServicesProvider(),
     new RepositoryProvider()
 ]);
-$app->setRoutes(
-    require_once $basePath . '/src/Http/Routing/routes.php'
-);
+
+/** @var list<RouteInterface> $routes */
+$routes = require_once __DIR__ . '/../src/Http/Routing/routes.php';
+$app->setRoutes($routes);
 $app->run();
