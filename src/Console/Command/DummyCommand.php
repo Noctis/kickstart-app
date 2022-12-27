7782b495cde1de2fb10043a00fbfffbee29f517e
Lucas Czejgis <lucas.czejgis@gmail.com>
2022-12-27 18:43:40 +0100

    Moved `DummyCommand` command name to attribute

--- src/Console/Command/DummyCommand.php
+++ src/Console/Command/DummyCommand.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace App\Console\Command;
 
 use Noctis\KickStart\Console\Command\AbstractCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(name: 'dummy:command')]
 final class DummyCommand extends AbstractCommand
 {
-    protected static $defaultName = 'dummy:command';
-
     protected function configure(): void
     {
         $this
